7a0c4225c2d2be7c28b4b6d0e551b355443227c4
ControlNet <smczx@hotmail.com>
2025-10-14 09:08:00 +1100

    feat: Add Google Analytics (GA4) tracking for production

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,19 @@
   let { children } = $props();
 </script>
 
+<svelte:head>
+  {#if import.meta.env.PROD}
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YQQ8ZW96YX"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-YQQ8ZW96YX');
+    </script>
+  {/if}
+  
+</svelte:head>
+
 {@render children()}
